758df49741130cb74185c6d50ed840e9a43e890d
Abdul Rahim K <abdulrahim.rymec@gmail.com>
2016-07-13 13:00:45 +0530

    Update jhj.java

--- jhj.java
+++ jhj.java
@@ -3,7 +3,7 @@ public class jhj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("snipe IT solution");
-System.out.println("Devops Team abdul rahim 123");
+System.out.println("Devops Team abdul rahim ");
 	}
 
 }
